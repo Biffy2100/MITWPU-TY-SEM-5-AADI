194ae6773d9f0f764e785dc1d587024b6df1c693
Biffy21 <biffyplaysmc@gmail.com>
2025-08-01 12:52:06 +0530

    Start of the year
    Getting some files setup

---
+++ Experiment 1/Demo.java
@@ -0,0 +1,8 @@
+public class Demo{
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+        System.out.println("This is a demo program.");
+        System.out.println("Welcome to the Java programming world!");
+    }
+}
+
