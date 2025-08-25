b70ab1a9c25bde0bd62f8a74f978b782e9bf8a97
Biffy21 <biffyplaysmc@gmail.com>
2025-08-25 08:33:44 +0530

    Java practicals added

---
+++ Java practicals/palindrome.java
@@ -0,0 +1,24 @@
+
+
+import java.util.*;
+
+
+public class palindrome{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string:");
+        String str = sc.nextLine();
+
+        StringBuffer sb = new StringBuffer(str);
+        sb.reverse();
+        String reversedStr = sb.toString();
+
+        if(str.equals(reversedStr)){
+            System.out.println("The string is a palindrome.");
+        } else {
+            System.out.println("The string is not a palindrome.");
+        }
+
+        sc.close();
+    }
+}
---
+++ Java practicals/vowel.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class vowel {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string:");
+        String str = sc.nextLine();
+
+        int count = 0;
+
+        for(int i = 0;i<str.length();i++){
+            char c = str.charAt(i);
+            if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
+               c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U'){
+                count++;
+            }
+        }
+        System.out.println("Number of vowels in the string: " + count);
+        sc.close();
+    }
+    
+}
