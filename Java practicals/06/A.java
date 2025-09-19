eb941c895248098d4ec9cf00820aba6cf9229179
Biffy21 <biffyplaysmc@gmail.com>
2025-09-19 18:03:30 +0530

    Exp 6 edited
    Code is not workign
    working on it from reference book

---
+++ Java practicals/06/A.java
@@ -0,0 +1,11 @@
+import useful.useme;
+import java.util.*;
+
+public class A {
+    public static void main(String[] args) {
+        useme u = new useme();
+        u.salary();
+        u.area();
+        u.percentage();
+    }
+}
---
+++ Java practicals/06/useful/useme.java
@@ -0,0 +1,15 @@
+package useful;
+
+public class useme {
+    void area() {
+        System.out.println("Area of class useme");
+    }
+
+    void salary() {
+        System.out.println("Salary of class useme");
+    }
+
+    void percentage() {
+        System.out.println("Percentage of class useme");
+    }
+}
