11e7db54b97c638bd64b3240fc2538319db9db5b
Biffy21 <biffyplaysmc@gmail.com>
2025-09-29 20:14:03 +0530

    eh

--- Java_practicals/06/expSix.java
+++ Java_practicals/06/expSix.java
@@ -1,8 +1,15 @@
-package ThePackage;
+try{package ThePackage;}catch(Exception e){}
 
 public class expSix {
-    public static void main(String[] args) {
-        useme u = new useme();
+    public static void main(String[] args) throws Exception {
+
+        try {
+            useme u = new useme();
+
+        } catch (MyException e) {
+            System.out.println(e);
+        }
+
         u.salary();
         u.area();
         u.percentage();
